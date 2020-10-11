ba867a92ee2e7f429f6960f2b35887b1563500d1
h13i32maru <h13i32maru@gmail.com>
2020-10-11 10:08:27 +0900

    feat(build): impl

--- script/build.ts
+++ script/build.ts
@@ -3,24 +3,42 @@ import path from 'path';
 import {episodes} from './episodes';
 import {EpisodeRender} from './EpisodeRender';
 
-// load template
-const templateIndexPath = path.resolve(__dirname, `../template/episode-index.html`);
-const templateIndexHTML = fs.readFileSync(templateIndexPath).toString();
-
 const episodeRender = new EpisodeRender();
 
-// make episode file
-for (const episode of episodes) {
-  const indexHTML = templateIndexHTML
-    .replace(/__TITLE__/g, `${episode.number}. ${episode.title} | CodeLunch.fm`)
-    .replace(/__DESC__/g, episode.desc)
-    .replace(/__URL__/g, `https://codelunch.fm/${episode.number}`)
-    .replace(/__NUMBER__/g, `${episode.number}`)
-    .replace(/__HTML__/g, episodeRender.renderEpisode(episode))
+// build index.html
+{
+  // load template
+  const templateIndexPath = path.resolve(__dirname, `../template/index.html`);
+  const templateIndexHTML = fs.readFileSync(templateIndexPath).toString();
 
-  const dirPath = path.resolve(__dirname, `../${episode.number}`);
-  fs.rmdirSync(dirPath, {recursive: true});
-  fs.mkdirSync(dirPath);
-  fs.writeFileSync(`${dirPath}/index.html`, indexHTML);
-  console.log(dirPath)
+  const indexHTML = templateIndexHTML
+    .replace(/__HTML__/, episodeRender.renderEpisodes(episodes));
+
+  const indexPath = path.resolve(__dirname, `../index.html`);
+  fs.writeFileSync(indexPath, indexHTML);
+  console.log(indexPath)
 }
+
+// build ${number}/index.html
+{
+  // load template
+  const templateIndexPath = path.resolve(__dirname, `../template/episode-index.html`);
+  const templateIndexHTML = fs.readFileSync(templateIndexPath).toString();
+
+  // make episode file
+  for (const episode of episodes) {
+    const indexHTML = templateIndexHTML
+      .replace(/__TITLE__/g, `${episode.number}. ${episode.title} | CodeLunch.fm`)
+      .replace(/__DESC__/g, episode.desc)
+      .replace(/__URL__/g, `https://codelunch.fm/${episode.number}`)
+      .replace(/__NUMBER__/g, `${episode.number}`)
+      .replace(/__HTML__/g, episodeRender.renderEpisode(episode))
+
+    const dirPath = path.resolve(__dirname, `../${episode.number}`);
+    fs.rmdirSync(dirPath, {recursive: true});
+    fs.mkdirSync(dirPath);
+    fs.writeFileSync(`${dirPath}/index.html`, indexHTML);
+    console.log(dirPath)
+  }
+}
+
