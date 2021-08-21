b9a6e30091d45660b61ade5ba201996e92bb6993
ryo.maruyama <ryo.maruyama@dr-ubie.com>
2021-08-21 14:22:51 +0900

    fix: nits

--- src/View/BodyView.tsx
+++ src/View/BodyView.tsx
@@ -11,10 +11,11 @@ export const BodyView: React.FC = (props) => {
           <div className='title'>CodeLunch.fm</div>
         </a>
         <div className='icons'>
-          <a href='https://twitter.com/h13i32maru'><img src='/image/twitter.png'/></a>
-          <a href='https://anchor.fm/s/77d9b6c/podcast/rss'><img src='/image/rss.png'/></a>
-          <a href='https://podcasts.apple.com/jp/podcast/codelunch-fm/id965805982'><img src='/image/podcast.png'/></a>
-          <a href='https://open.spotify.com/show/3vVss8nt0eT89tqrac0UAN'><img src='/image/spotify.png'/></a>
+          <a href='https://twitter.com/h13i32maru' target='_blank'><img src='/image/twitter.png'/></a>
+          <a href='https://anchor.fm/s/77d9b6c/podcast/rss' target='_blank'><img src='/image/rss.png'/></a>
+          <a href='https://podcasts.apple.com/jp/podcast/codelunch-fm/id965805982' target='_blank'><img
+            src='/image/podcast.png'/></a>
+          <a href='https://open.spotify.com/show/3vVss8nt0eT89tqrac0UAN' target='_blank'><img src='/image/spotify.png'/></a>
         </div>
         <div>Technical topics that software engineers talk about at lunch.</div>
       </header>
@@ -22,7 +23,8 @@ export const BodyView: React.FC = (props) => {
       {props.children}
 
       <footer>
-        Copyright 2013 - {new Date().getFullYear()} by <a href='https://twitter.com/h13i32maru'>Ryo Maruyama</a> all
+        Copyright 2013 - {new Date().getFullYear()} by <a href='https://twitter.com/h13i32maru' target='_blank'>Ryo
+        Maruyama</a> all
         right reserved.
       </footer>
     </body>
